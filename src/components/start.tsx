67a1e4046235bb6168df90e3770324d7e26aa6a6
Edo <edikhambardzumyan2@gmail.com>
2025-08-13 18:40:18 +0400

    New

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,12 +1,12 @@
 export default function Start(){
     return (
-        <div className="w-[395px] h-[700px] flex flex-col">
-            <div className="w-full h-[700px] flex overflow-hidden">
+        <div className="w-[390px] h-[698px] flex flex-col border-2">
+            <div className="w-full h-[698px] flex overflow-hidden">
            <img className="w-[100%] h-full relative top-30 z-5 object-cover " src="/img/men.png" alt="" />
            <img className="w-[300px] absolute top-12 left-4" src="/img/text.png" alt="" />
         </div>
-        <div className="w-full h-[80px] flex justify-center items-center">
-            <button className="px-20 py-3 bg-[#191919] text-white font-semibold rounded-[10px] shadow-md transition-all cursor-pointer">
+        <div className="w-full h-[78px] flex justify-center items-center pt-2">
+        <button className="px-20 py-3 bg-[#191919] text-white font-semibold rounded-[10px] shadow-md transition-all cursor-pointer">
           Get Started →
         </button>
         </div>
